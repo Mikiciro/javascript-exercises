a160c97407204569d474f12db6cf85a75f228f22
Mikiciro <cirollamiki@gmail.com>
2024-02-07 14:05:43 +0100

    modificato ma non funziona

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,14 @@
 var removeFromArray = function (array, ...args) {
-  const newArray = [];
-
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] !== args) {
+  let newArray = [];
+  for(let i = 0; i < array.length; i++){
+    if (array[i] !== args){
       newArray.push(array[i]);
-    }
+    }  
   }
   return newArray;
 };
 // Do not edit below this line
 module.exports = removeFromArray;
+
+//seguito questo esempio ma non funziona 
+//https://www.freecodecamp.org/news/how-to-remove-an-element-from-a-javascript-array-removing-a-specific-item-in-js/#how-to-remove-an-element-from-an-array-while-mutating-the-array
