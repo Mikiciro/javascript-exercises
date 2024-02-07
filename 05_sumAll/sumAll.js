2cd03f874699a15181d3e4c49dad33d112d8237d
Mikiciro <cirollamiki@gmail.com>
2024-02-07 14:50:08 +0100

    provato

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-    
+const sumAll = function(start, finish) {
+    let sum = 0;
+    let array = [start, finish]
+    for(i = start; i <= array.length; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
