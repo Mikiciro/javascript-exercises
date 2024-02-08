fac794d6fa227d6c9bdd5ce8f1a19e96f06bd15e
Mikiciro <cirollamiki@gmail.com>
2024-02-08 11:03:07 +0100

    risolto

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,18 +1,20 @@
 const leapYears = function (year) {
   if (year % 4 === 0) {
     if (year % 100 === 0) {
-        if (year % 400 === 0) {
-            return true;
-        }else {
-            return false;
-        }
-    }else{
+      if (year % 400 === 0) {
         return true;
+      } else {
+        return false;
+      }
+    } else {
+      return true;
     }
-  }else{
+  } else {
     return false;
   }
+  
 };
+
 
 // Do not edit below this line
 module.exports = leapYears;
